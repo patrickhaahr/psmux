9646ebfe315cfe6d3f76a32b4112d346603e8f3b
computersrmyfriends <computersrmyfriends@users.noreply.github.com>
2026-02-02 17:56:03 +0530

    chore: bump version to 0.2.2 for release with keyboard and color fixes

--- packages/chocolatey/tools/chocolateyinstall.ps1
+++ packages/chocolatey/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/marlocarlo/psmux/releases/download/v0.2.1/psmux-0.2.1-windows-x64.zip'
+$url64 = 'https://github.com/marlocarlo/psmux/releases/download/v0.2.2/psmux-0.2.2-windows-x64.zip'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
-  checksum64     = '7DB7142FD250A97967DCCBEEE8CD15761110130F15E667A47919A2D5F6603D13'
+  checksum64     = '5C824C38607F8F1DE5619703B5546048B37EC22B6F4F72FA62269B32A9ED8D19'
   checksumType64 = 'sha256'
 }
 
